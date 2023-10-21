1ae0efdfaf68e6eaf70a125e21a981befec045c6
Jordan Lee <jtlee@rmgj2.com>
2023-10-21 14:27:19 -0500

    posting and editing

---
+++ models/prompt.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const PromptScheme = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required."],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required."],
+  },
+});
+
+const Prompt = models.Prompt || model("Prompt", PromptScheme);
+
+export default Prompt;
